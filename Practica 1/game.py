58fbf5ffa8379ee963e9e3c951521eb7eb035f52
Nahuel Saroglia Gambino <nahuelgambino@gmail.com>
2024-03-21 16:22:06 -0300

    Agrego la ultima funcionalidad

--- Practica 1/game.py
+++ Practica 1/game.py
@@ -1,8 +1,7 @@
 import random
 
 # Lista de palabras posibles
-words = ["python", "programación", "computadora", "código", "desarrollo",
-"inteligencia"]
+words = ["python", "programacion", "computadora", "codigo", "desarrollo", "inteligencia"]
 
 # Elegir una palabra al azar
 secret_word = random.choice(words)
@@ -14,9 +13,40 @@ guessed_letters = []
 print("¡Bienvenido al juego de adivinanzas!")
 print("Estoy pensando en una palabra. ¿Puedes adivinar cuál es?")
 
-word_displayed = "_" * len(secret_word)
-# Mostrarla palabra parcialmente adivinada
+# Selector de dificultad
+print("Seleccione una dificultad")
+print("1. Facil")
+print("2. Media")
+print("3. Dificil")
+
+while True:
+    try:
+        difficulty = int(input("Ingrese el numero correspondiente al nivel de dificultad: "))
+        if difficulty == 1 or difficulty == 2 or difficulty == 3:
+            match difficulty:
+                case 1:
+                    word_displayed = ""
+                    for letter in secret_word:
+                        if letter in ["a", "e", "i", "o", "u"]:
+                            word_displayed += letter
+                        else: 
+                            word_displayed += "_" 
+                    break
+                case 2:
+                    word_displayed = secret_word[0] + "_" * (len(secret_word) - 2) + secret_word[-1]
+                    break
+                case 3:
+                    word_displayed = "_" * len(secret_word)
+                    break
+        else:
+            print("Valor invalido, porfavor ingrese un numero valido")
+    except ValueError:
+        print("Valor invalido, porfavor ingrese un numero valido")
+
 print(f"Palabra: {word_displayed}")
+for letter in word_displayed:
+    if letter in word_displayed:
+        guessed_letters.append(letter)
 
 while (number_failures < 10):
     # Pedir al jugador que ingrese una letra
@@ -50,7 +80,7 @@ while (number_failures < 10):
     print(f"Palabra: {word_displayed}")
     # Verificar si se ha adivinado la palabra completa
     if word_displayed == secret_word:
-        print(f"¡Felicidades! Has adivinado la palabra secreta:{secret_word}")
+        print(f"¡Felicidades! Has adivinado la palabra secreta: {secret_word}")
         break
 else:
     print(f"¡Oh no! Has agotado tus {number_failures} intentos.")
